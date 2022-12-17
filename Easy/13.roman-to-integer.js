c62ccb63073499f45b9bcd0a324443fe423dc050
asemabdou <asemhamdi97@gmail.com>
2022-12-17 17:01:42 +0300

    roman to integer

---
+++ Easy/13.roman-to-integer.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=13 lang=javascript
+ *
+ * [13] Roman to Integer
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    //create object js for roman to integer
+    let romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    let result = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (romanMap[s[i]] < romanMap[s[i+1]]) {
+            result -= romanMap[s[i]];
+        } else {
+            result += romanMap[s[i]];
+        }
+    }
+    return result;
+};
+// @lc code=end
+
