cb372a21c00a0ea3f5876c77c7b68b890b996368
Asem Hamdi <asemhamdi97@gmail.com>
2023-04-05 02:16:49 +0300

    20.valid-parentheses

--- Easy/20.valid-parentheses.js
+++ Easy/20.valid-parentheses.js
@@ -10,7 +10,43 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+  // We create a stack (we use an array in JavaScript) to remember open brackets.
+  const stack = [];
+
+  // This helper function checks if the open and close brackets match.
+  function bracketsMatch(open, close) {
+    if (open === '(' && close === ')') return true;
+    if (open === '{' && close === '}') return true;
+    if (open === '[' && close === ']') return true;
+    return false;
+  }
+
+  // We go through the string one character at a time.
+  for (const char of s) {
+
+    // If the character is an open bracket, we remember it in the stack.
+    if (char === '(' || char === '{' || char === '[') {
+      stack.push(char);
+    } 
+    // If the character is a close bracket, we check if it matches the last open bracket.
+    else {
+      // If the stack is empty, there's no open bracket to match, so the string is not valid.
+      if (stack.length === 0) {
+        return false;
+      }
+
+      // We take the last open bracket from the stack and check if it matches the close bracket.
+      const lastOpenBracket = stack.pop();
+      if (!bracketsMatch(lastOpenBracket, char)) {
+        // If the brackets don't match, the string is not valid.
+        return false;
+      }
+    }
+  }
+
+  // After we check all characters, the stack should be empty if the string is valid.
+  return stack.length === 0;
+
 };
 // @lc code=end
 
